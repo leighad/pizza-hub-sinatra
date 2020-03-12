17fc0f4b834abaced873fb6f19f9a2d2e75ecb53
leighad <cdversiontwo@gmail.com>
2020-03-11 17:01:56 -0700

    create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,18 @@
 
-# User.create(user_name: "pizzagirl415" email: "girl415@pizza.com" password: "1eatp1zza")
-# User.create(user_name: "pizzaguy510" email: "dude510@pizza.com" password: "8pizzanow")
+User.create(user_name: "pizzagirl415" email: "girl415@pizza.com" password: "1eatp1zza")
+User.create(user_name: "pizzaguy510" email: "dude510@pizza.com" password: "8pizzanow")
 
-# Pizza.create(pizza_name: )
+Pizza.create(pizza_name: "My favorite" origin: "Arizmendi" price: "24" user_id: 1)
+Pizza.create(pizza_name: "Cheese from the freezer" origin: "Home" price: "4" user_id: 1)
+Pizza.create(pizza_name: "Meat lover" origin: "Zachary's" price: "26" user_id: 2)
+Pizza.create(pizza_name: "Home-made original" origin: "Home" price: "8" user_id: 2)
+
+Ingredients.create(ingredient_name: "corn" amount: "extra" pizza_id: 1)
+Ingredients.create(ingredient_name: "poblano" amount: "average" pizza_id: 1)
+Ingredients.create(ingredient_name: "sauce" amount: "average" pizza_id: 2)
+Ingredients.create(ingredient_name: "cheese" amount: "average" pizza_id: 2)
+Ingredients.create(ingredient_name: "pepperoni" amount: "extra" pizza_id: 3)
+Ingredients.create(ingredient_name: "chorizo" amount: "light" pizza_id: 3)
+Ingredients.create(ingredient_name: "fresh mozzarella" amount: "light" pizza_id: 4)
+Ingredients.create(ingredient_name: "cornmeal crust" amount: "average" pizza_id: 4)
+
