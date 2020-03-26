3d76c26da81b2df29f75999b0c01cd471746feb8
leighad <cdversiontwo@gmail.com>
2020-03-25 23:54:56 -0700

    add get and post routes for login, create login form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,29 @@ class UsersController < ApplicationController
         erb :'/users/signup'
     end
 
+    get '/login' do 
+        erb :'/users/login'
+    end
+
     post '/signup' do
         user = User.create(params)
         if user.valid?
             session[:user_id] = user.id
             redirect to "/users/#{user.id}"
         else
+            #add flash error message
             redirect to 'signup'
+        end
+    end
+
+    post '/login' do 
+        user = User.find_by(user_name: params[:user_name])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect to "/users/#{user.id}"
+        else
+            #add flash error message
+            redirect to '/signup'
         end
     end
 end
