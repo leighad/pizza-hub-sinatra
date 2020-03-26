c7539be08b8f0e19689039d22ad08deb15e0731b
leighad <cdversiontwo@gmail.com>
2020-03-25 23:43:47 -0700

    add get and post routes for signup, create signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,15 @@
 class UsersController < ApplicationController
-    
+    get '/signup' do 
+        erb :'/users/signup'
+    end
+
+    post '/signup' do
+        user = User.create(params)
+        if user.valid?
+            session[:user_id] = user.id
+            redirect to "/users/#{user.id}"
+        else
+            redirect to 'signup'
+        end
+    end
 end
