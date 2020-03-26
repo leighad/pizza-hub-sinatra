49e78d98a4eaa8eda6be4fd7dfa50d7219ffbf11
leighad <cdversiontwo@gmail.com>
2020-03-25 23:47:12 -0700

    add user_name and email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
     has_secure_password
     has_many :pizzas
+
+    validates :user_name, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
+
 end
