e2e3f2e69c01a2f68d6e109f13a721d2c36973c3
leighad <cdversiontwo@gmail.com>
2020-03-28 21:17:30 -0700

    add redirect method with error to Helpers

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,6 +1,13 @@
 class Helpers
+
+    def self.redirect_if_not_logged_in
+        if !logged_in?
+            redirect "/login?error=You must be logged in to do that!"
+        end
+    end
+    
     def self.current_user(session)
-        User.find_by(id: session[:user_id])
+        User.find_by_id(session[:user_id])
     end
 
     def self.logged_in?(session)
